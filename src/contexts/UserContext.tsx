708edb49916b250531fc2bbe6f18a51af535d5fe
Sunyx <lorenzosanseverino2003@gmail.com>
2025-04-02 23:33:40 +0200

    context now use localstorage

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, { createContext, useContext, useState, ReactNode, useEffect } from "react";
+import axios from "axios";
+import Cookies from 'js-cookie';
 
 type User = {
   id: string;
@@ -15,7 +17,30 @@ type UserContextType = {
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(() => {
+    const cachedUser = localStorage.getItem('user');
+    return cachedUser ? JSON.parse(cachedUser) : null;
+  });
+
+  useEffect(() => {
+    const discordToken = Cookies.get('discord_token')
+    console.log(discordToken)
+    if (!discordToken || user) return
+
+      const fetchUser = async () => {
+          try {
+              const response = await axios.get<User>("http://localhost:3001/discord/me", {
+                  withCredentials: true,
+              });
+              localStorage.setItem('user', JSON.stringify(response.data))
+              setUser(response.data);
+          } catch (error) {
+              console.error("Errore nel fetch:", error);
+          }
+      };
+
+      fetchUser();
+  })
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
